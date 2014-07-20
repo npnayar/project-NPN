62715a8b2f7f9eaf571d30a311642c86f36f2377
Nitya Pandalai Nayar <nitya.nayar@gmail.com>
2014-07-20 11:59:41 -0400

    Changed variable name to number line

--- mean.py
+++ mean.py
@@ -5,8 +5,8 @@ n=0
 
 #Trying something new
 #Sum input values
-for num in open('data.txt'):
-	sum +=float(num)
+for numberline in open('data.txt'):
+	sum +=float(numberline)
 	n +=1
 
 print "This has got to be wrong:"
